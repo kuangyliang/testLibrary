611ec38569cb0a4013711d30c8a3c5e8b1da7144
zhouyiliang <ylzhou@etekcity.com.cn>
2019-02-01 10:39:59 +0800

    no message

---
+++ UseToTestLibrary/app/src/main/java/cn/zyl/com/usetotestlibrary/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.zyl.com.usetotestlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.zyl.com.mylibrary.Rxutils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Rxutils.
+    }
+}
---
+++ UseToTestLibrary/mylibrary/src/main/java/cn/zyl/com/mylibrary/Rxutils.java
@@ -0,0 +1,5 @@
+package cn.zyl.com.mylibrary;
+
+public class Rxutils {
+    public static final String rx ="我是正则";
+}
