94669d5d2c77749a417a6aba13a6013a7f385e1a
zhouyiliang <ylzhou@etekcity.com.cn>
2019-02-01 11:11:22 +0800

    重新来一遍

---
+++ UseToTestLibrary/app/src/main/java/cn/zyl/com/usetotestlibrary/Rxutils2.java
@@ -0,0 +1,5 @@
+package cn.zyl.com.usetotestlibrary;
+
+public class Rxutils2 {
+    public static final String rx ="我是正则";
+}
--- UseToTestLibrary/mylibrary/src/main/java/cn/zyl/com/mylibrary/Rxutils.java
+++
@@ -1,5 +0,0 @@
-package cn.zyl.com.mylibrary;
-
-public class Rxutils {
-    public static final String rx ="我是正则";
-}
